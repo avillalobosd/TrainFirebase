65db7491d284b1a633be285c993da313f100e512
abelardocbc <abelardocbc@gmail.com>
2018-06-10 21:40:00 -0500

    Final

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 
+var  nextTrain;
+var nextTrainTime;
 var config = {
     apiKey: "AIzaSyBOvL-WeYU1553U8Rv-9xZmjwMkz85KX4o",
     authDomain: "traincbc-474ec.firebaseapp.com",
@@ -20,6 +22,11 @@ $(".enviar").on("click", function () {
     console.log(destination);
     console.log(first);
     console.log(frequency);
+    $(".name").val("");
+    $(".destination").val("");
+    $(".first").val("");
+    $(".frequency").val("");
+
 
     firebase.database().ref().push({
         nombre: nombre,
@@ -45,9 +52,37 @@ database.ref().on("child_added", function(childSnapshot) {
     var destination = childSnapshot.val().destination;
     var first = childSnapshot.val().first;
     var frequency = childSnapshot.val().frequency;
-    console.log("1");
+    var firstTime = first;
+    var tFrequency = frequency;
 
-    $(".tabla").append("<tr><td>"+nombre+"</td><td>"+destination+"</td><td>"+first+"</td><td>"+frequency+"</td><td>"+50+"</td></tr>");
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+
+
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+ 
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    nextTrainTime="ARRIVAL TIME: " + moment(nextTrain).format("hh:mm");
+    console.log(nextTrain); 
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    var nextTrainTime="ARRIVAL TIME: " + moment(nextTrain).format("hh:mm");
+
+
+    $(".tabla").append("<tr><td>"+nombre+"</td><td>"+destination+"</td><td>"+first+"</td><td>"+frequency+"</td><td>"+nextTrainTime+"</td></tr>");
     
   
     // var i= '<div class="col s12 m6 l3"> <div style="height:300px; width:300px;"class="card medium"> <div class="card-image waves-effect waves-block waves-light"> <img style="heigth:300px, width:300px;"class="activator" src="'+fotoperro+'"> </div><div class="card-content"> <span class="card-title activator grey-text text-darken-4">'+nombreperro+ '<i class="material-icons right">more_vert</i> </span> </div> <div class="card-reveal"> <span class="card-title grey-text text-darken-4">'+nombreperro+'<i class="material-icons right">close</i> </span> <p> Dueño: '+owner+'</p><p> Celular: '+celular+'<a href="https://api.whatsapp.com/send?phone=52'+celular+'&text=hola,%20quisiera%20información%20del%20perrito,%20'+nombreperro+'%20que%20estás%20dando%20en%20adopcion?"> <img style="width:40px; height:40px;"border="0" alt="Enviar Whatsapp" src="assets/images/whatsapp.png" width="100" height="100"></a></p><p> Email: '+email+'</p><p> Raza: '+raza+'</p><p>'+descripcion+'</p> </div> </div> </div>'
